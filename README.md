e8113536599a69e3bed763620edeabdd27a89ffc
LonelyLok <40349145+LonelyLok@users.noreply.github.com>
2024-12-04 21:53:48 -0800

    add snack bar and readme (#1)

---
+++ README.md
@@ -0,0 +1,15 @@
+### Text to QR Code Generator
+A simple Electron application that converts text into a QR code. This app allows you to enter text, generate its corresponding QR code, copy the QR code image to your clipboard, and clear the input for new entries.
+
+### Features
+- Text Input: Enter any text you wish to convert into a QR code.
+- Generate QR Code: Convert the entered text into a QR code image.
+- Copy QR Code: Copy the generated QR code image to your clipboard.
+- Clear Input: Reset the text input and QR code display.
+
+### Technologies Used
+- Electron: Framework for building cross-platform desktop applications using web technologies.
+- React: JavaScript library for building user interfaces.
+- Material-UI (MUI): React components for faster and easier web development.
+- TypeScript: Adds static typing to JavaScript for better tooling and safer code.
+- Python: Used for generating QR codes via a backend script.
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { TextField, Typography, Stack, Button, Box } from '@mui/material';
+import {
+  TextField,
+  Typography,
+  Stack,
+  Button,
+  Box,
+  Snackbar,
+} from '@mui/material';
 
 function App() {
   const [text, setText] = useState<string>('');
   const [qrCode, setQrCode] = useState('');
+  const [snackBarOpen, setSnackBarOpen] = useState(false);
+  const [snackBarMessage, setSnackBarMessage] = useState('');
   const theme = createTheme({
     palette: {
       text: {
@@ -22,19 +31,30 @@ function App() {
     window.backendAPI.onQRGenerated((data) => {
       setQrCode(`data:image/png;base64,${data}`); // Set Base64 image source
     });
+    setSnackBarMessage('QR Code generated.');
+    setSnackBarOpen(true);
   };
 
   const handleCopyImage = () => {
-    if(!qrCode) {
+    if (!qrCode) {
       return;
     }
     window.backendAPI.writeImage(qrCode);
-  }
+    setSnackBarMessage('QR Code copied to clipboard.');
+    setSnackBarOpen(true);
+  };
 
   const handleClear = () => {
     setQrCode('');
     setText('');
-  }
+    setSnackBarMessage('Cleared.');
+    setSnackBarOpen(true);
+  };
+
+  const handSnackbarClose = () => {
+    setSnackBarOpen(false);
+    setSnackBarMessage('');
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -46,7 +66,7 @@ function App() {
           backgroundColor: '#343541',
         }}
       >
-        <Stack spacing={2} alignItems='center'  style={{ marginTop: '20px' }}>
+        <Stack spacing={2} alignItems='center' style={{ marginTop: '20px' }}>
           <Typography color='text.primary' variant='h4'>
             Text to QR Code
           </Typography>
@@ -59,22 +79,37 @@ function App() {
             sx={{
               label: { color: '#ffffff' },
               '& .MuiInputBase-root': { color: '#ffffff' },
-              fieldset : { borderColor: '#ffffff' },
+              fieldset: { borderColor: '#ffffff' },
             }}
           />
-          <Box display="flex" gap={2}>
-          <Button variant="contained" onClick={handleGetQR}>
-            Submit
-          </Button>
-          <Button variant="contained" color="secondary" onClick={handleCopyImage}>
-            Copy
-          </Button>
-          <Button variant="outlined" color="error" onClick={handleClear}>
-            Clear
-          </Button>
-        </Box>
+          <Box display='flex' gap={2}>
+            <Button variant='contained' onClick={handleGetQR}>
+              Submit
+            </Button>
+            <Button
+              variant='contained'
+              color='secondary'
+              onClick={handleCopyImage}
+            >
+              Copy
+            </Button>
+            <Button variant='outlined' color='error' onClick={handleClear}>
+              Clear
+            </Button>
+          </Box>
           {qrCode && <img src={qrCode} alt='Generated QR Code' />}
         </Stack>
+        <Snackbar
+          ContentProps={{
+            sx: {
+              backgroundColor: '#04758f'
+            }
+          }}
+          open={snackBarOpen}
+          autoHideDuration={3000}
+          onClose={handSnackbarClose}
+          message={snackBarMessage}
+        />
       </div>
     </ThemeProvider>
   );
